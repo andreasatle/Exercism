ad516f4cf4636640f3c3524520582b09450efc09
Andreas Atle <atle.andreas@gmail.com>
2021-05-01 12:45:38 -0500

    Added js-bank-account

--- javascript/bank-account/bank-account.js
+++ javascript/bank-account/bank-account.js
@@ -1,26 +1,53 @@
 export class BankAccount {
+
   constructor() {
-    throw new Error('Remove this statement and implement this function');
+    this.isOpen = false;
   }
 
   open() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.isOpen) {
+      throw new ValueError("Trying to open an already opened account.");
+    }
+    this.isOpen = true;
+    this.amount = 0;
   }
 
   close() {
-    throw new Error('Remove this statement and implement this function');
+    if (!this.isOpen) {
+      throw new ValueError("Trying to close an already closed account.");
+    }
+    this.isOpen = false;
   }
 
-  deposit() {
-    throw new Error('Remove this statement and implement this function');
+  deposit(amount) {
+    if (!this.isOpen) {
+      throw new ValueError("Trying to deposit in closed account.");
+    }
+    if (amount < 0) {
+      throw new ValueError("Trying to deposit a negative amount.");
+    }
+    this.amount += amount;
   }
 
-  withdraw() {
-    throw new Error('Remove this statement and implement this function');
+  withdraw(amount) {
+    if (!this.isOpen) {
+      throw new ValueError("Trying to deposit in closed account.");
+    }
+    if (this.amount < amount) {
+      throw new ValueError("Not enough money in account.");
+    }
+    if (amount < 0) {
+      throw new ValueError("Trying to withdraw a negative amount.");
+    }
+
+    this.amount -= amount;
   }
 
   get balance() {
-    throw new Error('Remove this statement and implement this function');
+    if (!this.isOpen) {
+      throw new ValueError("Trying to get the balance in closed account.");
+    }
+    return +this.amount;
   }
 }
 
