0887ae5d762d96d7a1fb8137d28d64e5011144bc
Andreas Atle <atle.andreas@gmail.com>
2021-05-01 09:39:07 -0500

    Added py-raindrops

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,16 @@
+# The rules of raindrops are that if a given number:
+#
+# has 3 as a factor, add 'Pling' to the result.
+# has 5 as a factor, add 'Plang' to the result.
+# has 7 as a factor, add 'Plong' to the result.
+# does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+def convert(number):
+    s = ""
+    if number % 3 == 0:
+        s += "Pling"
+    if number % 5 == 0:
+        s += "Plang"
+    if number % 7 == 0:
+        s += "Plong"
+
+    return s if len(s) > 0 else str(number)
