22406b6372e8f7cac4a7dbe928201ca177823b1e
Andreas Atle <atle.andreas@gmail.com>
2021-05-04 13:04:53 -0500

    Updated doc for rs-luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -11,33 +11,33 @@
 //! 4539 3195 0343 6467
 //! The first step of the Luhn algorithm is to double every second digit, starting from the right. We will be doubling
 //! 
-//! ```
+//! ```comment
 //! 4_3_ 3_9_ 0_4_ 6_6_
 //! ```
 //! If doubling the number results in a number greater than 9 then subtract 9 from the product. The results of our doubling:
 //! 
-//! ```
+//! ```comment
 //! 8569 6195 0383 3437
 //! ```
 //! Then sum all of the digits:
 //! 
-//! ```
+//! ```comment
 //! 8+5+6+9+6+1+9+5+0+3+8+3+3+4+3+7 = 80
 //! ```
 //! If the sum is evenly divisible by 10, then the number is valid. This number is valid!
 //! 
 //! # Example 2: invalid credit card number
-//! ```
+//! ```comment
 //! 8273 1232 7352 0569
 //! ```
 //! Double the second digits, starting from the right
 //! 
-//! ```
+//! ```comment
 //! 7253 2262 5312 0539
 //! ```
 //! Sum the digits
 //! 
-//! ```
+//! ```comment
 //! 7+2+5+3+2+2+6+2+5+3+1+2+0+5+3+9 = 57
 //! ```
 //! 57 is not evenly divisible by 10, so this number is not valid.
