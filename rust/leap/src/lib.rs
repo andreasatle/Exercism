9096d11c4808ce0dfbc0dde5792bc3dd0d6e69ea
Andreas Atle <atle.andreas@gmail.com>
2021-05-04 13:09:07 -0500

    Updated doc for rs-leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -2,7 +2,7 @@
 //! 
 //! The tricky thing here is that a leap year in the Gregorian calendar occurs:
 //! 
-//! ```
+//! ```comment
 //! on every year that is evenly divisible by 4
 //!   except every year that is evenly divisible by 100
 //!     unless the year is also evenly divisible by 400
