c808c11d1738ce31ada3a35477c4104a102c0e0d
Andreas Atle <atle.andreas@gmail.com>
2021-05-04 13:24:40 -0500

    Updated doc for rs-pythagorean-triplet

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,16 @@
+//! A Pythagorean triplet is a set of three natural numbers, ```{a, b, c}```, for which,
+//! 
+//! ```a**2 + b**2 = c**2```
+//! and such that,
+//! 
+//! ```a < b < c```
+//! For example,
+//! 
+//! ```3**2 + 4**2 = 9 + 16 = 25 = 5**2.```
+//! Given an input integer N, find all Pythagorean triplets for which ```a + b + c = N```.
+//! 
+//! For example, with N = 1000, there is exactly one Pythagorean triplet for which 
+//! ```a + b + c = 1000: {200, 375, 425}.```
 use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
