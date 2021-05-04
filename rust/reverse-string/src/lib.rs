ede1d1c44fd87107f3f0968de8d5834834e02b07
Andreas Atle <atle.andreas@gmail.com>
2021-05-04 13:31:27 -0500

    Updated doc for rs-reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,8 @@
+//! Reverse a string.
+//!
+//! For example: input: "cool" output: "looc".
+
+/// Return the string in reverse.
 pub fn reverse(input: &str) -> String {
     input.chars().rev().collect()
 }
