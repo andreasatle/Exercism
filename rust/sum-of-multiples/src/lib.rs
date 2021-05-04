075e56b23f126fe0c60e51025c43636bd44c7eb3
Andreas Atle <atle.andreas@gmail.com>
2021-05-04 13:39:53 -0500

    Updated doc for rs-sum-of-multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,16 +1,26 @@
+//! Given a number, find the sum of all the unique multiples of particular 
+//! numbers up to but not including that number.
+//! 
+//! If we list all the natural numbers below 20 that are multiples of 
+//! 3 or 5, we get 3, 5, 6, 9, 10, 12, 15, and 18.
+//! 
+//! The sum of these multiples is 78.
+
 use std::cmp;
 
+/// Returns the sum of all multiples of the given factors.
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut num = next_number(0, limit, factors);
+    let mut num = next_multiple(0, limit, factors);
     let mut sum = 0;
     while num < limit {
         sum += num;
-        num = next_number(num, limit, factors);
+        num = next_multiple(num, limit, factors);
     }
     sum
 }
 
-fn next_number(num: u32, limit: u32, factors: &[u32]) -> u32 {
+/// Compute the next multiple.
+fn next_multiple(num: u32, limit: u32, factors: &[u32]) -> u32 {
     let mut next = num + limit; // Make next too big...
     for f in factors.iter() {
         if *f == 0 {
