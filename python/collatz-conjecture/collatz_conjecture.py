ae4f59284cfea0e56b426434a03ae86d6ef4330e
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 13:02:05 -0500

    Updated doc for py-collatz-conjecture

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,4 +1,22 @@
-def steps(number: int):
+'''The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+
+Take any positive integer n. If n is even, divide n by 2 to get n / 2. 
+If n is odd, multiply n by 3 and add 1 to get 3n + 1. 
+Repeat the process indefinitely. The conjecture states that no matter 
+which number you start with, you will always reach 1 eventually.
+
+Given a number n, return the number of steps required to reach 1.
+
+Starting with n = 12, the steps would be as follows:
+
+12, 6, 3, 10, 5, 16, 8, 4, 2, 1
+
+Resulting in 9 steps. So for input n = 12, the return value would be 9.
+'''
+
+
+def steps(number: int) -> int:
+    '''Count the number of steps in the Collatz-sequence before reaching 1.'''
     if number <= 0:
         raise ValueError(f"Argument: number = {number} is too small.")
 
