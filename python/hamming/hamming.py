4b7b5c6582f2d299a13a152b8dac06b3dff1e1ae
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 14:11:16 -0500

    Updated doc for py-hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,23 @@
-def distance(strand_a, strand_b):
+'''Calculate the Hamming Distance between two DNA strands.
+
+Your body is made up of cells that contain DNA. Those cells 
+regularly wear out and need replacing, which they achieve by
+dividing into daughter cells. In fact, the average human body
+experiences about 10 quadrillion cell divisions in a lifetime!
+
+When cells divide, their DNA replicates too. Sometimes during
+this process mistakes happen and single pieces of DNA get encoded
+with the incorrect information. If we compare two strands of DNA
+and count the differences between them we can see how many mistakes
+occurred. This is known as the "Hamming Distance".
+
+Exceptions: When two strands are of different length,
+a ValueError is raised.
+'''
+
+
+def distance(strand_a: str, strand_b: str) -> int:
+    '''returns the Hamming distance of to strands.'''
     if len(strand_a) != len(strand_b):
         raise ValueError("Different lengths of input.")
 
