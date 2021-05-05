3069aa64ce6124a02139be04626e3fa2d0ec2def
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 14:43:40 -0500

    Updated doc for py-hello-world

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -1,2 +1,9 @@
-def hello():
+'''The classical introductory exercise. Just say "Hello, World!".
+
+"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+'''
+
+
+def hello() -> str:
+    '''Returns the string "Hello, World!"'''
     return "Hello, World!"
