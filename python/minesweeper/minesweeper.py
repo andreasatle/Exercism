b6b7f2b6eb054ad9b85809cf219b0be7ebed187b
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 16:48:25 -0500

    Updated doc for py-minesweeper

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,4 +1,25 @@
-def annotate(minefield):
+'''Add the mine counts to a completed Minesweeper board.
+
+Minesweeper is a popular game where the user has to find
+the mines using numeric hints that indicate how many mines
+are directly adjacent (horizontally, vertically, diagonally)
+to a square.
+
+In this exercise you have to create some code that counts the
+number of mines adjacent to a given empty square and replaces
+that square with the count.
+
+The board is a rectangle composed of blank space (' ')
+characters. A mine is represented by an asterisk ('*') character.
+
+If a given space has no adjacent mines at all,
+leave that square blank.
+
+Exception ValueError is raised if the minefield is not rectangular.
+'''
+
+
+def annotate(minefield: list[str]) -> list[str]:
     output = []
     for i in range(len(minefield)):
         row = ''
