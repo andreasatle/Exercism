bda568c0a691b3b37ac7829b486e39ce981d961e
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 18:44:41 -0500

    Updated doc for py-scrabble-score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,5 +1,18 @@
+'''Given a word, compute the scrabble score for that word.
 
-def score(word):
+Letter                           Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+'''
+
+
+def score(word: str) -> int:
+    ''' Returns the scrabble score for a given word.'''
     points = 0
     for ch in word.upper():
         if ch in 'AEIOULNRST':
