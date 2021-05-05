51332a4cf52d78f6833a97817b772264eb1c1240
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 15:04:14 -0500

    Updated doc for py-high-scores

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,12 +1,25 @@
-def latest(scores):
+'''Manage a game player's High Score list.
+
+Your task is to build a high-score component of the classic
+Frogger game, one of the highest selling and addictive games
+of all time, and a classic of the arcade era. Your task is to
+write methods that return the highest score from the list, the
+last added score and the three highest scores.
+'''
+
+
+def latest(scores: list[int]) -> int:
+    '''returns the latest score from the Frogger game.'''
     return scores[-1]
 
 
-def personal_best(scores):
+def personal_best(scores: list[int]) -> int:
+    '''returns the personal best score from the Frogger game.'''
     return max(scores)
 
 
-def personal_top_three(scores):
+def personal_top_three(scores: list[int]) -> list[int]:
+    '''returns the top three scores from the Frogger game.'''
     # Special cases
     if len(scores) == 1:
         return scores
