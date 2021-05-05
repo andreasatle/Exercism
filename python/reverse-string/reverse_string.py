79f121051d3202fff2ca294d8a10745a67501ec2
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 18:40:34 -0500

    Updated doc for py-reverse-string

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,4 +1,11 @@
-def reverse(text: str):
+'''Reverse a string
+
+For example: input: "cool" output: "looc"
+'''
+
+
+def reverse(text: str) -> str:
+    '''Return the string in reverse.'''
     chars = list(text)
     for i in range(len(text)//2):
         chars[i], chars[-i-1] = chars[-i-1], chars[i]
