166a8361b815ca8b4c2c9226a819e638bfc900f7
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 15:31:47 -0500

    Updated doc for py-kindergarten-garden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,14 +1,62 @@
+'''Determine plants for each child in the class.
+
+Given a diagram, determine which plants each child in the kindergarten class is responsible for.
+
+The kindergarten class is learning about growing plants. The teacher thought it would be a good idea to give them actual seeds, plant them in actual dirt, and grow actual plants.
+
+They've chosen to grow grass, clover, radishes, and violets.
+
+To this end, the children have put little cups along the window sills, and planted one type of plant in each cup, choosing randomly from the available types of seeds.
+
+[window][window][window]
+........................ # each dot represents a cup
+........................
+There are 12 children in the class:
+
+Alice, Bob, Charlie, David,
+Eve, Fred, Ginny, Harriet,
+Ileana, Joseph, Kincaid, and Larry.
+Each child gets 4 cups, two on each row. Their teacher assigns cups to the children alphabetically by their names.
+
+The following diagram represents Alice's plants:
+
+[window][window][window]
+VR......................
+RG......................
+In the first row, nearest the windows, she has a violet and a radish. In the second row she has a radish and some grass.
+
+Your program will be given the plants from left-to-right starting with the row nearest the windows. From this, it should be able to determine which plants belong to each student.
+
+For example, if it's told that the garden looks like so:
+
+[window][window][window]
+VRCGVVRVCGGCCGVRGCVCGCGV
+VRCCCGCRRGVCGCRVVCVGCGCV
+Then if asked for Alice's plants, it should provide:
+
+Violets, radishes, violets, radishes
+While asking for Bob's plants would yield:
+
+Clover, grass, clover, clover
+'''
+
+
 class Garden:
-    def __init__(self, diagram, students=None):
-        self.create_flower_map()
-        self.create_diagram(diagram)
-        self.create_students(students)
-        pass
+    '''Gargen manager class that keeps track of which child 
+    has which flowers in the garden..'''
+
+    def __init__(self, diagram: str, students: list[str] = None):
+        '''Initializes the Garden class.'''
+        self._create_flower_map()
+        self._create_diagram(diagram)
+        self._create_students(students)
 
     def __str__(self):
+        '''Definition of string-output for the class.'''
         return f"row 1: {self.row1},\nrow 2: {self.row2},\nstudents: {self.students},\nflower_map: {self.flower_map}"
 
-    def create_diagram(self, diagram):
+    def _create_diagram(self, diagram: str):
+        '''Help function to create the diagram.'''
         rows = diagram.split("\n")
         if len(rows) != 2 or len(rows[0]) != len(rows[1]):
             raise ValueError(f"Can't process diagram: {diagram}!")
@@ -21,19 +69,24 @@ class Garden:
                 raise ValueError(
                     f"Flower: {flower} not in flower-map: {self.flower_map}")
 
-    def create_students(self, students):
+    def _create_students(self, students: list[str]):
+        '''Help function to initialize students.'''
         if students is None:
             students = ["Alice", "Bob", "Charlie", "David",
                         "Eve", "Fred", "Ginny", "Harriet",
                         "Ileana", "Joseph", "Kincaid", "Larry"]
         self.students = sorted(students)
 
-    def create_flower_map(self):
+    def _create_flower_map(self):
+        '''Help function to initialize flower map.'''
         self.flower_map = {
             'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'
         }
 
-    def plants(self, student):
+    def plants(self, student: str) -> list[str]:
+        '''Returns the 4 plants for the given child.
+        If the child is not in the class, we throw a ValueError.'''
+
         # Check that student is valid
         if student not in self.students:
             raise ValueError(
