3c39a87ce129779c0738477fe2e5f18b2fba2773
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 16:38:29 -0500

    Updated doc for py-matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,15 +1,26 @@
+''' Matrix handler class.
+
+Given a string representing a matrix of numbers,
+return the rows and columns of that matrix.
+'''
+
+
 class Matrix:
-    def __init__(self, matrix_string):
+    '''Matrix class, returns row and columns of matrix.'''
+
+    def __init__(self, matrix_string: str):
+        '''Initializer of Matrix class.'''
         self.matrix = []
         for row in matrix_string.split("\n"):
             self.matrix.append([int(num) for num in row.split()])
-        print(self.matrix)
 
-    def row(self, index):
+    def row(self, index: int) -> list[int]:
+        '''Returns row given by index.'''
         # It is safer to return a copy of the row, in case you change it.
         return self.matrix[index-1].copy()
 
-    def column(self, index):
+    def column(self, index: int) -> list[int]:
+        '''Returns column given by index.'''
         col = []
         for row in self.matrix:
             col.append(row[index-1])
