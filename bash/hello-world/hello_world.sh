d14f6eaa118613f5ea569daa1013198db51bacae
Andreas Atle <atle.andreas@gmail.com>
2021-05-06 12:55:24 -0500

    Added sh-hello-world

---
+++ bash/hello-world/hello_world.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# The classical introductory exercise.
+# Just say "Hello, World!".
+#
+# "Hello, World!" is the traditional first program 
+# for beginning programming in a new language or
+# environment.
+
+echo "Hello, World!"
