7b66c9b246e61c8dce65e269c6e7f48f67173783
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 19:17:10 -0500

    Updated doc for py-two-fer

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,18 @@
-def two_fer(name="you"):
+'''Two-fer or 2-fer is short for two for one.
+
+One for you and one for me.
+
+Given a name, return a string with the message:
+
+One for X, one for me.
+Where X is the given name.
+
+However, if the name is missing, return the string:
+
+One for you, one for me.
+'''
+
+
+def two_fer(name: str = "you") -> str:
+    '''Returns a string "One for {name}, one for me."'''
     return f"One for {name}, one for me."
