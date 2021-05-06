fa8b2d657d3df43f30cec0de0a1005c273907eb5
Andreas Atle <atle.andreas@gmail.com>
2021-05-06 09:08:59 -0500

    Updated doc for js-pascals-triangle

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,3 +1,23 @@
+/**
+ * Compute Pascal's triangle up to a given number of rows.
+ * 
+ * In Pascal's Triangle each number is computed by adding the numbers to the right and left of the current position in the previous row.
+ * 
+ * <p>     1
+ * <p>    1 1
+ * <p>   1 2 1
+ * <p>  1 3 3 1
+ * <p> 1 4 6 4 1
+ * @file
+ * @author Andreas Atle, atle.andreas@gmail.com
+ */
+
+/**
+ * Compute the first n rows in Pascals triangle.
+ * @func
+ * @param {number} n Number of rows.
+ * @returns {number[][]} The first n rows of Pascals triangle.
+ */
 export const rows = (n) => {
   let pascal = [];
   if (n == 0) {
