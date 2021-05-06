e6d9a0415f6f892b1c54746119a4525b04b332e3
Andreas Atle <atle.andreas@gmail.com>
2021-05-06 16:00:22 -0500

    Added sh-error-handling

---
+++ bash/error-handling/error_handling.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Implement various kinds of error handling and resource management.
+# 
+# An important point of programming is how to handle errors and close
+# resources even if errors occur.
+# 
+# This exercise requires you to handle various errors. Because error handling
+# is rather programming language specific you'll have to refer to the tests
+# for your track to see what's exactly required.
+
+[[ $# == 1 ]] && echo "Hello, ""$1" || echo "Usage: error_handling.sh <person>"
