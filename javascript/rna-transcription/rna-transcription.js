de73898fb8544f20c2b0717c49874ed1fbf1a5ed
Andreas Atle <atle.andreas@gmail.com>
2021-05-06 09:40:37 -0500

    Updated doc for js-rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,3 +1,22 @@
+/** 
+ * Given a DNA strand, return its RNA complement (per RNA transcription).
+ * 
+ * Both DNA and RNA strands are a sequence of nucleotides.
+ * 
+ * The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G) and thymine (T).
+ * 
+ * The four nucleotides found in RNA are adenine (A), cytosine (C), guanine (G) and uracil (U).
+ * 
+ * Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement:
+ * 
+ * <p>G -> C
+ * <p>C -> G
+ * <p>T -> A
+ * <p>A -> U
+ * @file
+ * @author Andreas Atle, atle.andreas@gmail.com
+ */
+
 // Mapping use for conversion from DNA to RNA.
 const DNA_TO_RNA = {
   'G': 'C',
@@ -6,7 +25,12 @@ const DNA_TO_RNA = {
   'A': 'U'
 }
 
-// toRna returns the RNA complement to a given DNA strand.
+/** 
+ * toRna returns the RNA complement to a given DNA strand.
+ * @func
+ * @param {string} dna DNA strand.
+ * @returns {string} Corresponding RNA strand.
+ */
 export const toRna = (dna) => {
   let rna = ""
   for (let i = 0; i < dna.length; i++) {
