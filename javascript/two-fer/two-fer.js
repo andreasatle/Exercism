e58f55038358ffe96493431be4abe34dff2e9b67
Andreas Atle <atle.andreas@gmail.com>
2021-05-06 10:35:41 -0500

    Updated doc for js-two-fer

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,8 +1,24 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/**
+ * Two-fer or 2-fer is short for two for one. One for you and one for me.
+ * 
+ * Given a name, return a string with the message:
+ * 
+ * <p>One for name, one for me.
+ * <p>Where "name" is the given name.
+ * 
+ * However, if the name is missing, return the string:
+ * 
+ * <p>One for you, one for me.
+ * @file
+ * @author Andreas Atle, atle.andreas@gmail.com
+ */
 
+/**
+ * Returns a string 'One for {name}, one for me'.
+ * @func
+ * @param {string} [name='you'] The name of the person.
+ * @returns {string} the wanted string.
+ */
 export const twoFer = (name = "you") => {
   return "One for " + name + ", one for me.";
 };
