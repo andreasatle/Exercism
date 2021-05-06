d098d6ffa0d09482402d9509ade54628ca6fb01f
Andreas Atle <atle.andreas@gmail.com>
2021-05-05 19:23:03 -0500

    Updated doc for py-word-count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,6 +1,23 @@
-def count_words(sentence):
+'''Given a phrase, count the occurrences of each word in that phrase.
+
+For the purposes of this exercise you can expect that a word will always be one of:
+
+A number composed of one or more ASCII digits (ie "0" or "1234") OR
+A simple word composed of one or more ASCII letters (ie "a" or "they") OR
+A contraction of two simple words joined by a single apostrophe (ie "it's" or "they're")
+When counting words you can assume the following rules:
+
+The count is case insensitive (ie "You", "you", and "YOU" are 3 uses of the same word)
+The count is unordered; the tests will ignore how words and counts are ordered
+Other than the apostrophe in a contraction all forms of punctuation are ignored
+The words can be separated by any form of whitespace (ie "\t", "\n", " ")
+'''
+
+
+def count_words(sentence: str) -> dict[str, int]:
+    '''Returns a dictionary with the case-insensitive word-counts.'''
     word_cnt = dict()
-    for word in filter_and_split(sentence):
+    for word in _filter_and_split(sentence):
         if word in word_cnt:
             word_cnt[word] += 1
         else:
@@ -8,7 +25,8 @@ def count_words(sentence):
     return word_cnt
 
 
-def filter_and_split(sentence):
+def _filter_and_split(sentence: str) -> list[str]:
+    '''Filter and split the sentence into a list of words.'''
     # Make lower case and create list of chars
     chars = [ch for ch in sentence.lower()]
 
