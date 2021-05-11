74b7ce5c6c28ac53a418a8d4c57fa2d04619a623
Andreas Atle <atle.andreas@gmail.com>
2021-05-11 10:29:10 -0500

    Added go-hello-world

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,12 @@
+// Package greeting, contains the classical introductory exercise, "Hello, World!".
+//
+// Just say "Hello, World!".
+//
+// "Hello, World!" is the traditional first program for
+// beginning programming in a new language or environment.
+package greeting
+
+// HelloWorld returns "Hello, World."
+func HelloWorld() string {
+	return "Hello, World!"
+}
