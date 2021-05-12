40c7176f2a33c6fc5fb94a5fe54f750e92b68e30
Andreas Atle <atle.andreas@gmail.com>
2021-05-12 11:35:25 -0500

    Updated c-resistor-color

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -5,16 +5,6 @@ int color_code(resistor_band_t color) {
 }
 
 resistor_band_t * colors(void) {
-    static resistor_band_t A[10];
-    A[0] = BLACK;
-    A[1] = BROWN;
-    A[2] = RED;
-    A[3] = ORANGE;
-    A[4] = YELLOW;
-    A[5] = GREEN;
-    A[6] = BLUE;
-    A[7] = VIOLET;
-    A[8] = GREY;
-    A[9] = WHITE;
+    static resistor_band_t A[10] = {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
     return A;
 }
