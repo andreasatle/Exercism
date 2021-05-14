8cbc9f01bcb045edf290b12bc72b85fe5e64fce0
Andreas Atle <atle.andreas@gmail.com>
2021-05-14 17:37:30 -0500

    Updated c-nucleotide-count

--- c/nucleotide-count/src/nucleotide_count.c
+++ c/nucleotide-count/src/nucleotide_count.c
@@ -1,28 +1,24 @@
 #include "nucleotide_count.h"
 
-enum indices {INDEX_A, INDEX_C, INDEX_G, INDEX_T};
 char *count(const char *dna_strand) {
     char *output = malloc(40);
-    int counter[4] = {0, 0, 0, 0};
+    int a, c, g, t;
+    a = c = g = t = 0;
 
     for (unsigned long i = 0; i < strlen(dna_strand); i++) {
         if (dna_strand[i] == 'A')
-            counter[INDEX_A]++;
+            a++;
         else if (dna_strand[i] == 'C')
-            counter[INDEX_C]++;
+            c++;
         else if (dna_strand[i] == 'G')
-            counter[INDEX_G]++;
+            g++;
         else if (dna_strand[i] == 'T')
-            counter[INDEX_T]++;
+            t++;
         else {
             output[0] = '\0';
             return output;
         }
     }
-    sprintf(output, "A:%d C:%d G:%d T:%d", 
-        counter[INDEX_A],
-        counter[INDEX_C],
-        counter[INDEX_G],
-        counter[INDEX_T]);
+    sprintf(output, "A:%d C:%d G:%d T:%d", a, c, g, t);
     return output;
 }
