f1714380af7d70a6bb42a0ef669de0beabeea648
Andreas Atle <atle.andreas@gmail.com>
2021-05-17 10:58:01 -0500

    Added go-hamming

---
+++ go/hamming/hamming.go
@@ -0,0 +1,40 @@
+// Package hamming - Calculate the Hamming Distance between two DNA strands.
+//
+// Your body is made up of cells that contain DNA. Those cells regularly wear out
+// and need replacing, which they achieve by dividing into daughter cells. In fact,
+// the average human body experiences about 10 quadrillion cell divisions
+// in a lifetime!
+//
+// When cells divide, their DNA replicates too. Sometimes during this process
+// mistakes happen and single pieces of DNA get encoded with the incorrect
+// information. If we compare two strands of DNA and count the differences
+// between them we can see how many mistakes occurred. This is known as the
+// "Hamming Distance".
+//
+// We read DNA using the letters C,A,G and T. Two strands might look like this:
+//  GAGCCTACTAACGGGAT
+//  CATCGTAATGACGGCCT
+//  ^ ^ ^  ^ ^    ^^
+// They have 7 differences, and therefore the Hamming Distance is 7.
+package hamming
+
+import "errors"
+
+func Distance(a, b string) (int, error) {
+
+	ar := []rune(a)
+	br := []rune(b)
+
+	if len(ar) != len(br) {
+		return 0, errors.New("Size mismatch of strings")
+	}
+
+	cnt := 0
+	for i := 0; i < len(ar); i++ {
+		if ar[i] != br[i] {
+			cnt++
+		}
+	}
+
+	return cnt, nil
+}
